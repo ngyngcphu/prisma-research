08f81fbe100005c13eb8e2d7821e89fe3d2cba7e
ngyngcphu <nguyenngocphu225@gmail.com>
2023-09-27 21:02:08 +0700

    feat(migrate): migrate schema to sql and add creat-action

--- src/seed.ts
+++ src/seed.ts
@@ -4,7 +4,51 @@ import { add } from 'date-fns'
 const prisma = new PrismaClient()
 
 // A `main` function so that we can use async/await
-async function main() {}
+async function main() {
+  await prisma.testResult.deleteMany({})
+  await prisma.courseEnrollment.deleteMany({})
+  await prisma.test.deleteMany({})
+  await prisma.user.deleteMany({})
+  await prisma.course.deleteMany({})
+
+  const grace = await prisma.user.create({
+    data: {
+      email: 'phu.nguyen2310@hcmut.edu.vn',
+      firstName: 'Phu',
+      lastName: 'Nguyen',
+      social: {
+        facebook: 'zinunar',
+        github: 'ngyngcphu'
+      }
+    }
+  });
+
+  const weekFromNow = add(new Date(), { days: 7 })
+  const twoWeekFromNow = add(new Date(), { days: 14 })
+  const monthFromNow = add(new Date(), { days: 28 })
+
+  const course = await prisma.course.create({
+    data: {
+      name: 'CRUD with Prisma',
+      tests: {
+        create: [
+          {
+            date: weekFromNow,
+            name: 'First test',
+          },
+          {
+            date: twoWeekFromNow,
+            name: 'Second test',
+          },
+          {
+            date: monthFromNow,
+            name: 'Final exam',
+          },
+        ],
+      },
+    },
+  })
+}
 
 main()
   .catch((e: Error) => {
@@ -13,5 +57,5 @@ main()
   })
   .finally(async () => {
     // Disconnect Prisma Client
-    await prisma.disconnect()
+    await prisma.$disconnect()
   })
